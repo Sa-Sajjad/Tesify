968a1edc4db3f3fbbdc85b5216adad2381e23277
KaeruShi <farel.ardiansyah404@gmail.com>
2023-02-18 14:15:39 +0700

    Test

---
+++ app/src/main/java/kaerushi/weeabooify/uwuify/fragment/FragmentSC.java
@@ -0,0 +1,45 @@
+package kaerushi.weeabooify.uwuify.fragment;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import kaerushi.weeabooify.uwuify.R;
+
+public class FragmentSC extends Fragment {
+
+    TextView title, desc;
+    Button btnVariant;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return (ViewGroup)inflater.inflate(R.layout.fragment_schicksal, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        title = view.findViewById(R.id.variant_title);
+        desc = view.findViewById(R.id.variant_detail);
+        btnVariant = view.findViewById(R.id.btnVariant);
+
+        title.setText("Weeabooify\nSchicksal");
+        desc.setText("Substratum For A10, Designed with Modern UI and Anime Style. Include QS Style, Settings, Lockscreen, Volume, etc.");
+        btnVariant.setOnClickListener(view1 -> {
+            String url = "https://t.me/weeabooify/68";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        });
+    }
+}
---
+++ app/src/main/java/kaerushi/weeabooify/uwuify/fragment/FragmentUWU.java
@@ -0,0 +1,43 @@
+package kaerushi.weeabooify.uwuify.fragment;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import kaerushi.weeabooify.uwuify.R;
+
+public class FragmentUWU extends Fragment {
+    TextView title, desc;
+    Button btnVariant;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return (ViewGroup)inflater.inflate(R.layout.fragment_uwu, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        title = view.findViewById(R.id.variant_title);
+        desc = view.findViewById(R.id.variant_detail);
+        btnVariant = view.findViewById(R.id.btnVariant);
+
+        title.setText("Weeabooify\nUwU");
+        desc.setText("Substratum for A10, Uwuify alternative but substratum version. Include Quick Settings Style.");
+        btnVariant.setOnClickListener(view1 -> {
+            String url = "https://t.me/weeabooify/233";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        });
+    }
+}
